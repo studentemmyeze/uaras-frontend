cb261f3140ebc0f77c2f5fbddd8e6566ec9b013f
Chukwuemeka Eze <c.eze@outlook.com>
2023-08-15 21:35:30 +0100

    sorted modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import {MatDividerModule} from '@angular/material/divider';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatTableModule} from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+// import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatInputModule} from '@angular/material/input';
+import {MatTooltipModule} from '@angular/material/tooltip';
+
+import {MatChipsModule} from '@angular/material/chips';
+
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+
+import {MatRadioModule} from '@angular/material/radio';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
+import {MatSliderModule} from '@angular/material/slider'
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent
@@ -12,6 +48,29 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     BrowserAnimationsModule,
     MatGridListModule,
+    CommonModule, MatDialogModule,
+    MatDividerModule,
+    HttpClientModule,
+    MatToolbarModule, MatButtonModule,
+    MatMenuModule,
+    MatChipsModule, MatRadioModule,
+    MatSnackBarModule, MatButtonToggleModule,
+    MatIconModule, MatListModule,
+    MatBottomSheetModule, MatCardModule,
+    MatTableModule,
+    MatSelectModule,
+    MatFormFieldModule, FormsModule,
+    MatGridListModule, MatExpansionModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatAutocompleteModule,
+    MatSlideToggleModule, MatTooltipModule,
+    ReactiveFormsModule, MatProgressSpinnerModule,
+    MatProgressBarModule, MatTabsModule,
+    MatStepperModule, MatBadgeModule,
+    MatPaginatorModule,
+    MatSliderModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
